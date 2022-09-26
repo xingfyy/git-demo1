feebf8d46a01a734602669d3e6fd95074c8bdbcd
xingfyy <114299199+xingfyy@users.noreply.github.com>
2022-09-26 17:45:11 +0800

    pull commit

--- src/main/java/com/hr/gitdome/GitTest.java
+++ src/main/java/com/hr/gitdome/GitTest.java
@@ -10,6 +10,6 @@ public class GitTest {
         System.out.println("master git4");
         System.out.println("hot git4");
         System.out.println("push git4");
-
+        System.out.println("pull git4");
     }
 }
