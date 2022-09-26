7bba2d0c12bcbabb7a180152e5e4a0537608e542
星枫月影 <1105569394@qq.com>
2022-09-26 17:00:37 +0800

    first commit

---
+++ src/main/java/com/hr/gitdome/GitTest.java
@@ -0,0 +1,9 @@
+package com.hr.gitdome;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("r");
+
+    }
+}
