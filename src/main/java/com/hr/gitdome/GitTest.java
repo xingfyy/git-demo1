0a6f205bbd2434b5921c8e92abefdfb09b1ef9f3
星枫月影 <1105569394@qq.com>
2022-09-26 17:03:00 +0800

    scode commit

--- src/main/java/com/hr/gitdome/GitTest.java
+++ src/main/java/com/hr/gitdome/GitTest.java
@@ -4,6 +4,8 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("r");
+        System.out.println("hello git1");
+        System.out.println("r1");
 
     }
 }
